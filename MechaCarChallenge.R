27226a4b76d7b405ee62af577773455b658cff60
McKenzie Kilburn <70282854+mckenziekkilburn@users.noreply.github.com>
2021-01-20 09:37:57 -0600

    adding files.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,14 @@ total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI
 
 # create lot summary df using groupby and summarize functions
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+
+#Deliverable 3 
+# Using t.test() and subset()
+sample_table <- suspension_coil %>% sample_n(50)
+# compare sample versus pop means
+t.test(sample_table$PSI,mu=mean(suspension_coil$PSI)) 
+#Individual Lots 
+t.test(subset(suspension_coil$PSI, suspension_coil$Manufacturing_Lot == "Lot1"), mu=total_summary$Mean) 
+t.test(subset(suspension_coil$PSI, suspension_coil$Manufacturing_Lot == "Lot2"), mu=total_summary$Mean) 
+t.test(subset(suspension_coil$PSI, suspension_coil$Manufacturing_Lot == "Lot3"), mu=total_summary$Mean)
